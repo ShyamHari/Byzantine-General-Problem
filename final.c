83325b5d91de7d222cc124f02ad7382f56986754
Shyam Hari <shyamu.hari@gmail.com>
2021-05-11 18:54:33 -0400

    Working recursive Byzantine Generals Problem

--- final.c
+++ final.c
@@ -14,15 +14,37 @@ bool loyal0[] = { true, true, false };
 bool loyal1[] = { true, false, true, true };
 bool loyal2[] = { true, false, true, true };
 bool loyal3[] = { true, false, true, true, false, true, true };
+bool loyal4[] = { true, true, true };//no traitors
+bool loyal5[] = { true, true, true, true, true, true, true};//no traitors max size
+bool loyal6[] = { true, true, true, false, true, true, true};//traitor is broadcaster 1 traitor total 
+bool loyal7[] = { true, true, true, true, true, false, true};//traitor is reciever 1 traitor total
+bool loyal8[] = { true, false, true, true, true, true, true};//traitor is reporter 1 traitor total
+bool loyal9[] = { false, false, false, true, true, true, true};//3 traitors 7 gen
+bool loyal10[] = { true, true, true, true, false, false, true };//2 traitors 1 reciever 1 commander
+bool loyal11[] = { false, true, true, false, true, true, true };//2 traitors 1 commander 1 reporter
+bool loyal12[] = { true, true, true, true, false};//1 traitor 5 generals, traitor is commander
+bool loyal13[] = { false, true, true, true, true}; //1 traitor 5 generals, traitor is reciever
 
-#define N_TEST 4
+#define N_TEST 14
 
 test_t tests[N_TEST] = {
 	{ sizeof(loyal0)/sizeof(loyal0[0]), loyal0, 1, 'R', 0 },
 	{ sizeof(loyal1)/sizeof(loyal1[0]), loyal1, 2, 'R', 0 },
 	{ sizeof(loyal2)/sizeof(loyal2[0]), loyal2, 2, 'A', 1 },
-	{ sizeof(loyal3)/sizeof(loyal3[0]), loyal3, 6, 'R', 0 }
+	{ sizeof(loyal3)/sizeof(loyal3[0]), loyal3, 6, 'R', 0 },
+	{ sizeof(loyal4)/sizeof(loyal4[0]), loyal4, 1, 'R', 0 },
+	{ sizeof(loyal5)/sizeof(loyal5[0]), loyal5, 6, 'A', 3 },
+	{ sizeof(loyal6)/sizeof(loyal6[0]), loyal6, 2, 'R', 3 },
+	{ sizeof(loyal7)/sizeof(loyal7[0]), loyal7, 6, 'A', 0 },
+	{ sizeof(loyal8)/sizeof(loyal8[0]), loyal8, 1, 'R', 4 },
+	{ sizeof(loyal9)/sizeof(loyal9[0]), loyal9, 1, 'R', 0 },
+	{ sizeof(loyal10)/sizeof(loyal10[0]), loyal10, 2, 'R', 4 },
+	{ sizeof(loyal11)/sizeof(loyal11[0]), loyal11, 3, 'R', 0 },
+	{ sizeof(loyal12)/sizeof(loyal12[0]), loyal12, 0, 'A', 4 },
+	{ sizeof(loyal13)/sizeof(loyal13[0]), loyal13, 2, 'R', 3 }
+
 };
+
 
 #define MAX_GENERALS 7
 uint8_t ids[MAX_GENERALS] = { 0, 1, 2, 3, 4, 5, 6 };
--- general.c
+++ general.c
@@ -2,11 +2,52 @@
 #include "general.h"
 
 // add any #includes here
+#include <string.h>
 
 // add any #defines here
 
+// might conflict with final.c
+#define MAX_GENERALS 7
+
+#define MAX_TRAITORS 2
+
+// max number of messages per recursion level
+#define MAX_MSGS0 1 
+#define MAX_MSGS1 5
+#define MAX_MSGS2 4
+
+// message sizes per recursion level
+#define MSG_SIZE0 4
+#define MSG_SIZE1 6
+#define MSG_SIZE2 8
+
 // add global variables here
 
+// keep track of generals that messages have been received from (per general) in 2D array? 
+int receivedFrom[MAX_GENERALS][MAX_TRAITORS];
+
+// id of reporter 
+uint8_t rep; 
+// id of sender
+uint8_t sender;
+// number of generals ... might conflict with definition in final.c 
+uint8_t n;
+// number of traitors 
+uint8_t m = 0;
+
+// level of recursion
+uint8_t recurse[MAX_GENERALS];
+
+// will store traitors 
+uint8_t traitors[MAX_TRAITORS];
+bool isFromBroadcast[MAX_GENERALS];
+
+// queue array 
+osMessageQueueId_t queues[MAX_GENERALS][MAX_TRAITORS+1];
+osSemaphoreId_t done[MAX_GENERALS];
+osSemaphoreId_t ready[MAX_GENERALS];
+
+// osSemaphoreId_t ready, done;
 
 /** Record parameters and set up any OS and other resources
   * needed by your general() and broadcast() functions.
@@ -16,6 +57,36 @@
   * return true if setup successful and n > 3*m, false otherwise
   */
 bool setup(uint8_t nGeneral, bool loyal[], uint8_t reporter) {
+	// store number of generals globally 
+	n = nGeneral; 
+	
+	// find number of traitors and store indices globally 
+	for(int i = 0; i < n; i++) {
+		if(!loyal[i]) {
+			traitors[m] = i;
+			m++;
+		}
+	}		
+	
+	// assert n > 3*m before proceeding 
+	if(!c_assert(n > 3*m)) {	
+		// reset m (since it is global)
+		m = 0;
+		return false;
+	}
+	
+	// generals communicate via message queues 
+	for(int i = 0; i < n; i++) {
+		done[i] = osSemaphoreNew(1, 0, NULL);
+		ready[i] = osSemaphoreNew(1, 0, NULL);
+		for(int j = 0; j < m+1; j++) {
+			queues[i][j] = osMessageQueueNew(MAX_MSGS0, 10, NULL);
+		}
+	}
+	
+	// store reporter index globally 
+	rep = reporter;
+	
 	return true;
 }
 
@@ -24,6 +95,19 @@ bool setup(uint8_t nGeneral, bool loyal[], uint8_t reporter) {
   * dynamically allocated by setup().
   */
 void cleanup(void) {
+	// delete semaphores 
+	//osSemaphoreDelete(ready);
+	//osSemaphoreDelete(done);
+	
+	// delete message queues
+	for(int i = 0; i < n; i++) {
+		osSemaphoreDelete(done[i]);
+		for(int j = 0; j < m+1; j++) 
+			osMessageQueueDelete(queues[i][j]);
+	}
+	
+	// reset number of traitors 
+	m = 0; 
 }
 
 
@@ -34,7 +118,118 @@ void cleanup(void) {
   * command: either 'A' or 'R'
   * sender: general sending the command to other n-1 generals
   */
-void broadcast(char command, uint8_t commander) {
+void broadcast(char command, uint8_t commander) {	
+	// store sender index globally for reference 
+	sender = commander;
+		
+	// prepare initial message for queues 
+	char message[MSG_SIZE0], pref[3];
+	pref[0] = '0' + commander;
+	pref[1] = ':';
+	pref[2] = '\0';
+	
+	// find if sender is loyal
+	bool senderLoyal = true;
+	for(int i = 0; i < m; i++) {
+		if(commander == traitors[i]) 
+			senderLoyal = false;
+	}
+
+	// case 1: sender is loyal
+	if(senderLoyal) {
+		for(int i = 0; i < n; i++) {
+			strcpy(message, pref);
+			// don't send to self!
+			if(i != commander) {
+				strcat(message, &command);
+				// send message to generals via message queues 
+				osMessageQueuePut(queues[i][m], &message, 0, 0); 
+			}
+		}
+	}
+	
+	// case 2: sender is a traitor
+	else {
+		for(int i = 0; i < n; i++) {
+			// strcpy rewrites message 
+			strcpy(message, pref);
+			if(i != commander) {
+				// send 'R' to even-numbered generals and 'A' to odd-numbered
+				i % 2 == 0 ? strcat(message, "R") : strcat(message, "A");
+				
+				// put message in queue 
+				osMessageQueuePut(queues[i][m], &message, 0, 0); 
+			}
+		}
+	}
+	
+	// signal semaphore when command ready 
+	// osSemaphoreRelease(ready);
+	
+	// wait semaphores until OM done 
+	for(int i = 0; i < n; i++) {
+		if(i != commander) osSemaphoreAcquire(done[i], osWaitForever);
+	}
+	
+	// newline to be nice 
+	printf("\n");
+}
+
+void om(uint8_t lvl, char msg[], int id) {
+	// strings store sent and received messages
+	char pref[3] = " ", send[MSG_SIZE2] = " ", received[MSG_SIZE2] = " ";
+	
+	// preface 
+	pref[0] = '0' + id;
+	pref[1] = ':';
+	pref[2] = '\0';
+	
+	// check if loyal
+	bool isLoyal = true;
+	for(int i = 0; i < m; i++) {
+		if(id == traitors[i]) 
+			isLoyal = false;
+	}
+	
+	// final recursion level OM(0): just print. 
+	if(lvl == 0) {
+		// print if reporter
+		if(id == rep) {
+			printf(" %s ", msg);
+		}
+	}
+	
+	// m > 0
+	else {
+		for(int i = 0; i < n; i++) {
+			// check if general id is in msg using strchr
+			if(i != id && strchr(msg, '0' + i) == NULL) {
+				// message begins with "id:"
+				strcpy(send, pref);
+				
+				// if loyal just append received message
+				if(isLoyal) strcat(send, msg);
+				
+				// traitor 
+				else {
+					// copy all of message characters except last 
+					strncat(send, msg, strlen(msg)-1);
+					
+					// even-numbered sends 'R', odd sends 'A'
+					id % 2 == 0 ? strcat(send, "R") : strcat(send, "A");
+				}
+				
+				// send message to lower level
+				osMessageQueuePut(queues[i][lvl-1], send, 0, 0);
+
+				// get messages 
+				osMessageQueueGet(queues[id][lvl-1], received, NULL, osWaitForever);
+				
+				// recurse 
+				om(lvl-1, received, id);
+			}
+		}
+	}
 }
 
 
@@ -45,6 +240,39 @@ void broadcast(char command, uint8_t commander) {
   * should output the messages received in OM(0).
   * idPtr: pointer to general's id number which is in [0,n-1]
   */
-void general(void *idPtr) {
+void general(void *idPtr) {	
+	// dereference ptr to grab id 
 	uint8_t id = *(uint8_t *)idPtr;
+	
+	// prepare initial message for queues 
+	char received[MSG_SIZE0];
+	
+	// get original message from queue
+	osMessageQueueGet(queues[id][m], &received, NULL, osWaitForever);
+	
+	// number of recursion levels
+	int recurse = m; 
+	
+	// call recursive OM algorithm
+	om(recurse, received, id);
+	
+ 	osSemaphoreRelease(done[id]);
 }
+
+// OM algorithm 
+/*
+
+Andrew's suggested approach from help session:
+
+if m == 0:
+	if id == r:
+		print msg
+
+else (m > 0): 
+	command = "id:0:R"
+	send to other generals
+	receive msgs from other generals
+	for each received msg rm:
+		om(m-1, rm, id)
+
+*/
